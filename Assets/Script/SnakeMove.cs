05b834e8544e3fb02e9bf76129b1b4fd3be7e498
duynguyen2709 <naduy.hcmus@gmail.com>
2017-09-20 16:50:02 +0700

    Backup code before changing snake moving

--- Assets/Script/SnakeMove.cs
+++ Assets/Script/SnakeMove.cs
@@ -7,22 +7,23 @@ public class SnakeMove : MonoBehaviour
 
     public float Speed = 4f;
     private Vector3 Direction = Vector3.left;
+    public float headDistance = 3f;
     public float clockwise = 1000.0f;
     public float counterClockwise = -5.0f;
     public GameObject cube;
+
     Vector3 newBodyPos;
     int bodyCount = 1;
-
+    List<GameObject> bodyPart;
     // Use this for initialization
     void Start()
     {
-       
+        bodyPart = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -39,42 +40,44 @@ public class SnakeMove : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
+       
         Vector3 newPos = new Vector3();
         switch (col.gameObject.name)
         {
             case "Right_Border":
-
+                // Change parent transform
                 newPos = GameObject.Find("Left_Border").transform.position;
-                newPos.x += 2f;
-                newPos.y = 0.5f;
-                newPos.z = this.transform.position.z;
-                transform.position = newPos;              
-                
-                break;
-            case "Left_Border":
-
-                newPos = GameObject.Find("Right_Border").transform.position;
-                newPos.x -= 2f;
+                newPos.x += 3f;
                 newPos.y = 0.5f;
                 newPos.z = this.transform.position.z;
                 transform.position = newPos;
 
                 break;
+            case "Left_Border":
+               
+                // Change parent transform
+                newPos = GameObject.Find("Right_Border").transform.position;
+                newPos.x -= 3f;
+                newPos.y = 0.5f;
+                newPos.z = this.transform.position.z;
+                transform.position = newPos;
 
+                break;
             case "Top_Border":
-
+                
+                // Change parent transform
                 newPos = GameObject.Find("Bottom_Border").transform.position;
-                newPos.z += 2f;
+                newPos.z += 3f;
                 newPos.y = 0.5f;
                 newPos.x = this.transform.position.x;
                 transform.position = newPos;
 
                 break;
-
             case "Bottom_Border":
-
+                
+                // Change parent transform
                 newPos = GameObject.Find("Top_Border").transform.position;
-                newPos.z -= 2f;
+                newPos.z -= 3f;
                 newPos.y = 0.5f;
                 newPos.x = this.transform.position.x;
                 transform.position = newPos;
@@ -82,15 +85,13 @@ public class SnakeMove : MonoBehaviour
                 break;
             case "FoodPrefab(Clone)":
 
-
                 newBodyPos = this.transform.position - bodyCount * transform.forward;
                 bodyCount++;
-
-                Instantiate(cube, newBodyPos, this.transform.rotation, this.transform);
+                bodyPart.Add(Instantiate(cube, newBodyPos, this.transform.rotation, this.transform) as GameObject);
                 Destroy(col.gameObject);
                 break;
         }
-        
+
     }
 
 
